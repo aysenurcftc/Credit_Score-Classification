40cc06592381d06f99cc177543823ee89ad58252
aysenur <aysenurfortuna@gmail.com>
2024-07-01 23:15:34 +0300

    add cli

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 
 #Constants
-RANDOM_SEED = 6
+RANDOM_SEED = 42
 
 
 run_mode = "training"
-dataset = "default_of_credit_card_clients" # datasets -> "uci_credit_approval" , "statlog_german_credit_data", "loan", "statlog_australian_credit_approval", "default_of_credit_card_clients"
+dataset = "uci_credit_approval" # datasets -> "uci_credit_approval" , "statlog_german_credit_data", "loan", "statlog_australian_credit_approval", "default_of_credit_card_clients"
 
 
-model_type = "logistic regression" # types -> "logistic regression", "NN"
+model_type = "NN" # types -> "logistic regression", "NN"
 imputing_type_numerical = "median" # types -> median, mean
 imputing_type_categorical = "mode" # types -> mode
-split_ratio = 0.8
+split_ratio = 0.7
 
 #data path
-filepath = "data/default_of_credit_card_clients.csv" 
+filepath = "data/uci_credit_approval.csv" 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from random import shuffle
 import config
 from data_operations.data_preprocesing import  data_imputing_categorical, data_imputing_numerical,   encode_features, label_encoding, min_max_scaler, read_data, split_data, standardize_features, uniform_discretization_transform
 from models.logistic_regression_nn import LogisticRegression
@@ -5,9 +6,18 @@ from models.model import train_model
 from models.nn_model import NnModel
 from sklearn.metrics import accuracy_score
 import numpy as np
+from sklearn.utils import resample
+import argparse
 
-
-def main():
+def main(args):
+   
+    config.run_mode = args.run_mode
+    config.dataset = args.dataset
+    config.model_type = args.model_type
+    config.imputing_type_numerical = args.imputing_type_numerical
+    config.imputing_type_categorical = args.imputing_type_categorical
+    config.split_ratio = args.split_ratio
+    config.filepath = args.filepath
     
     if config.run_mode == "training":
          
@@ -16,22 +26,16 @@ def main():
          if config.dataset == "uci_credit_approval":
             data= read_data(config.filepath)
             x_train,y_train, x_test,  y_test = split_data(data)
-            x_train, y_train, x_test, y_test = data_imputing_categorical(x_train, y_train ,x_test, y_test)
-            x_train, y_train, x_test, y_test = data_imputing_numerical(x_train,y_train, x_test,  y_test)
+            
+            x_train, y_train, x_test, y_test = data_imputing_numerical(x_train, y_train, x_test, y_test)
+            x_train, y_train, x_test, y_test = data_imputing_categorical(x_train, y_train, x_test, y_test)
             
             x_train, x_test = encode_features(x_train, x_test)
             x_train, x_test = standardize_features(x_train, x_test)
             
-            unique_train, counts_train = np.unique(y_train, return_counts=True)
-            unique_test, counts_test = np.unique(y_test, return_counts=True)
-            print(counts_train)
-            print(counts_test)
-            
-            
             model = train_model(x_train, y_train.values, x_test, y_test.values)
             
             
-
          
          elif config.dataset == "statlog_german_credit_data":
             data = read_data(config.filepath)
@@ -78,10 +82,8 @@ def main():
    
             x_train, y_train, x_test, y_test = split_data(data)
             
-            unique_train, counts_train = np.unique(y_train, return_counts=True)
-            unique_test, counts_test = np.unique(y_test, return_counts=True)
             
-   
+
             
             x_train, x_test = standardize_features(x_train, x_test)
             model = train_model(x_train, y_train, x_test, y_test)
@@ -110,7 +112,19 @@ def main():
          
          
 if __name__ == '__main__':
-    main()
+   
+    parser = argparse.ArgumentParser(description="Train a model on a specified dataset.")
+    parser.add_argument('--run_mode', type=str, default="training", help="Mode to run the script in, e.g., training or testing")
+    parser.add_argument('--dataset', type=str, default="uci_credit_approval", help="Dataset to use for training")
+    parser.add_argument('--model_type', type=str, default="NN", help="Type of model to use, e.g., 'logistic regression' or 'NN'")
+    parser.add_argument('--imputing_type_numerical', type=str, default="median", help="Imputing strategy for numerical features")
+    parser.add_argument('--imputing_type_categorical', type=str, default="mode", help="Imputing strategy for categorical features")
+    parser.add_argument('--split_ratio', type=float, default=0.7, help="Train-test split ratio")
+    parser.add_argument('--filepath', type=str, default="data/uci_credit_approval.csv", help="Path to the dataset file")
+    
+    args = parser.parse_args()
+    main(args)
+   
 
         
         
